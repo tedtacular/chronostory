a075a9046d27c8d08ce702b93549ff9125109cd7
tedtacular <theo.chiong@gmail.com>
2016-12-03 09:33:44 -0500

    first commit

---
+++ js/chronostory.js
@@ -0,0 +1,48 @@
+$(document).ready(function(){
+/*var you = prompt("What's your name?");*/
+
+$('.all-secs').hide();
+
+$('#exposition').show();
+
+/* - EXPOSITION TRIGGERS - */
+
+/* setTimeout(function() {
+  $("#trigger").fadeOut().empty();
+}, 5000);
+*/
+
+$("#next").click(function(){
+    $("#exposition").hide();
+    $("#exposition2").show();
+});
+
+$("#door1").click(function(){
+    $("#choice").hide();
+    $("#cipher").show();
+});
+
+$("#door2").click(function(){
+    $("#choice").hide();
+    $("#source").show();
+});
+
+$("#access").click(function(){
+  var password = document.getElementById('password').value;
+  if (password == "rawr") {
+    $("#cipher").hide();
+    $("#source").show();
+  }
+});
+
+$("#access2").click(function(){
+  var password = document.getElementById('password2').value;
+  if (password == "rawr") {
+    $("#source").hide();
+    $("#exposition").show();
+  }
+});
+
+
+
+});
